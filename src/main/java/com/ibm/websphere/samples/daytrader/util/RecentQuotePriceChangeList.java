7330c9ee6e92938885bb47d9a5ac95011cd82629
Joe McClure <jdmcclur@us.ibm.com>
2025-10-15 10:44:56 -0500

    Refactor websocket testing

--- src/main/java/com/ibm/websphere/samples/daytrader/util/RecentQuotePriceChangeList.java
+++ src/main/java/com/ibm/websphere/samples/daytrader/util/RecentQuotePriceChangeList.java
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corporation 2019.
+ * (C) Copyright IBM Corporation 2019, 2025.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,12 +54,13 @@ public class RecentQuotePriceChangeList  {
     int symbolNumber = Integer.parseInt(quoteData.getSymbol().substring(2));
 
     if ( symbolNumber < TradeConfig.getMAX_QUOTES() * TradeConfig.getListQuotePriceChangeFrequency() * 0.01) {
-      list.add(0, quoteData);
-
-      // Add stock, remove if needed
-      if(list.size() > maxSize) {
-        list.remove(maxSize);
-      }      
+      synchronized(list) {
+        list.add(0, quoteData);
+        // Add stock, remove if needed
+        if(list.size() > maxSize) {
+          list.remove(maxSize);
+        }
+      }
       quotePriceChangeEvent.fireAsync("quotePriceChange for symbol: " + quoteData.getSymbol(), NotificationOptions.builder().setExecutor(mes).build());
     }
     return true;
